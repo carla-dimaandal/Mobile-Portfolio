4604269d5aeadab7e78f8a8bd1db631091c50011
unknown <carlafrancescadimaandal29@gmail.com>
2024-07-12 07:51:07 +0800

    class

--- lib/widgets/educ.dart
+++ lib/widgets/educ.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class BookTab extends StatelessWidget {
-  const BookTab({Key? key}) : super(key: key);
+class EducTab extends StatelessWidget {
+  const EducTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
